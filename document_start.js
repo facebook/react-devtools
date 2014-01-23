43135bc84ce683b8b5414532aab5d43bc458bf4b
Ben Alpert <spicyjalapeno@gmail.com>
2014-01-22 18:17:10 -0800

    Set __REACT_DEVTOOLS_GLOBAL_HOOK__ before page

---
+++ document_start.js
@@ -0,0 +1,14 @@
+// Inject a `__REACT_DEVTOOLS_GLOBAL_HOOK__` global so that React can detect that the
+// devtools are installed (and skip its suggestion to install the devtools).
+var js = (
+  "Object.defineProperty(" +
+    "window, '__REACT_DEVTOOLS_GLOBAL_HOOK__', {value: true}" +
+  ")"
+);
+
+// This script runs before the <head> element is created, so we add the script
+// to <html> instead.
+var script = document.createElement('script');
+script.textContent = js;
+document.documentElement.appendChild(script);
+script.parentNode.removeChild(script);
