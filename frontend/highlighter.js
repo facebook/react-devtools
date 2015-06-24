81048d28801d322d9bb18e7f19cbcf5f57c2148a
Jared Forsyth <jared@jaredforsyth.com>
2015-06-24 14:35:49 -0700

    move highlighter away from canvas

--- frontend/highlighter.js
+++ frontend/highlighter.js
@@ -99,36 +99,50 @@ function makeMagnifier() {
 var overlayStyles = {
   background: 'rgba(120, 170, 210, 0.7)',
   padding: 'rgba(77, 200, 0, 0.3)',
-  margin: 'rgba(255, 132, 0, 0.3)',
+  margin: 'rgba(255, 155, 0, 0.3)',
   border: 'rgba(255, 200, 50, 0.3)',
 };
 
-function drawAround(ctx, left, top, width, height, what, dims) {
-  var mTop = dims[what + 'Top'];
-  var mLeft = dims[what + 'Left'];
-  var mRight = dims[what + 'Right'];
-  var mBottom = dims[what + 'Bottom'];
-  ctx.fillRect(left, top - mTop, width, mTop);
-  ctx.fillRect(left - mLeft, top - mTop, mLeft, height + mTop + mBottom);
-  ctx.fillRect(left + width, top - mTop, mRight, height + mTop + mBottom);
-  ctx.fillRect(left, top + height, width, mBottom);
+function setStyle(node, style) {
+  for (var name in style) {
+    node.style[name] = style[name];
+  }
+}
+
+function boxWrap(dims, what, node) {
+  setStyle(node, {
+    borderTopWidth: dims[what + 'Top'] + 'px',
+    borderLeftWidth: dims[what + 'Left'] + 'px',
+    borderRightWidth: dims[what + 'Right'] + 'px',
+    borderBottomWidth: dims[what + 'Bottom'] + 'px',
+    borderStyle: 'solid',
+  });
 }
 
 class Overlay {
   constructor(window) {
-    this.node = window.document.createElement('canvas');
-    this.node.style.pointerEvents = 'none';
-    this.node.style.position = 'fixed';
-    this.node.style.top = 0;
-    this.node.style.bottom = 0;
-    this.node.style.left = 0;
-    this.node.style.right = 0;
-    this.node.width = window.innerWidth;
-    this.node.height = window.innerHeight;
-    this.ctx = this.node.getContext('2d');
-    this.width = this.node.width;
-    this.height = this.node.height;
-    window.document.body.appendChild(this.node);
+    var doc = window.document;
+    this.container = doc.createElement('div');
+    this.node = doc.createElement('div');
+    this.border = doc.createElement('div');
+    this.padding = doc.createElement('div');
+    this.content = doc.createElement('div');
+
+    this.border.style.borderColor = overlayStyles.border;
+    this.padding.style.borderColor = overlayStyles.padding;
+    this.content.style.backgroundColor = overlayStyles.background;
+
+    setStyle(this.node, {
+      borderColor: overlayStyles.border,
+      pointerEvents: 'none',
+      position: 'fixed',
+    });
+
+   this.container.appendChild(this.node);
+   this.node.appendChild(this.border);
+   this.border.appendChild(this.padding);
+   this.padding.appendChild(this.content);
+   doc.body.appendChild(this.container);
   }
 
   remove() {
@@ -137,24 +151,21 @@ class Overlay {
 
   inspect(node) {
     var pos = nodePos(node);
-    this.ctx.clearRect(0,0,this.width,this.height);
     var dims = getElementDimensions(node);
-    var iW = node.offsetWidth - dims.borderLeft - dims.borderRight;
-    var iH = node.offsetHeight - dims.borderTop - dims.borderBottom;
-    var iX = pos.left + dims.borderLeft;
-    var iY = pos.top + dims.borderTop;
-    var iiW = iW - dims.paddingLeft - dims.paddingRight;
-    var iiH = iH - dims.paddingTop - dims.paddingBottom;
-    var iiX = iX + dims.paddingLeft;
-    var iiY = iY + dims.paddingTop;
-    this.ctx.fillStyle = overlayStyles.background;
-    this.ctx.fillRect(iiX, iiY, iiW, iiH);
-    this.ctx.fillStyle = overlayStyles.margin;
-    drawAround(this.ctx, pos.left, pos.top, node.offsetWidth, node.offsetHeight, 'margin', dims);
-    this.ctx.fillStyle = overlayStyles.border;
-    drawAround(this.ctx, iX, iY, iW, iH, 'border', dims);
-    this.ctx.fillStyle = overlayStyles.padding;
-    drawAround(this.ctx, iiX, iiY, iiW, iiH, 'padding', dims);
+
+    boxWrap(dims, 'margin', this.node);
+    boxWrap(dims, 'border', this.border);
+    boxWrap(dims, 'padding', this.padding);
+
+    setStyle(this.content, {
+      width: node.offsetWidth - dims.borderLeft - dims.borderRight - dims.paddingLeft - dims.paddingRight,
+      height: node.offsetHeight - dims.borderTop - dims.borderBottom - dims.paddingTop - dims.paddingBottom,
+    });
+
+    setStyle(this.node, {
+      top: pos.top - dims.marginTop,
+      left: pos.left - dims.marginLeft,
+    });
   }
 }
 
