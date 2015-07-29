a8e7126fca079192bf044adc2df311a1316d901b
Jared Forsyth <jared@jaredforsyth.com>
2015-07-29 10:07:01 -0700

    WIP magic panel

---
+++ shells/chrome/src/magic.js
@@ -0,0 +1,259 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
+'use strict';
+
+var React = require('react');
+var Bridge = require('../../../agent/Bridge');
+var Container = require('../../../frontend/Container');
+var NativeStyler = require('../../../plugins/ReactNativeStyle/ReactNativeStyle.js');
+var Store = require('../../../frontend/Store');
+var keyboardNav = require('../../../frontend/keyboardNav');
+
+var consts = require('../../../agent/consts');
+
+import type {DOMEvent} from '../../../frontend/types';
+
+type Props = {
+  alreadyFoundReact: boolean,
+  inject: () => void,
+  checkForReact: (cb: (isReact: boolean) => void) => void,
+  reload: () => void,
+
+  // optionals
+  showComponentSource: ?() => void,
+  reloadSubscribe: ?() => () => void,
+  showAttrSource: ?() => void,
+  executeFn: ?() => void,
+  selectElement: ?() => void,
+  getNewSelection: ?() => void,
+};
+
+class Panel extends React.Component {
+  _teardownWall: ?() => void;
+  _keyListener: ?(e: DOMEvent) => void;
+  _checkTimeout: ?number;
+  _unMounted: boolean;
+  _bridge: ?Bridge;
+  _store: Store;
+  _unsub: ?() => void;
+
+  props: Props;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {loading: true, isReact: this.props.alreadyFoundReact};
+    this._unMounted = false;
+    window.panel = this;
+  }
+
+  getChildContext(): Object {
+    return {
+      store: this._store,
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.alreadyFoundReact) {
+      this.inject();
+    } else {
+      this.lookForReact();
+    }
+
+    if (this.props.reloadSubscribe) {
+      this._unsub = this.props.reloadSubscribe(() => this.reload());
+    }
+  }
+
+  componentWillUnmount() {
+    this._unMounted = true;
+    if (this._unsub) {
+      this._unsub();
+    }
+  }
+
+  reload() {
+    if (this._unsub) {
+      this._unsub();
+    }
+    this.teardown();
+    if (!this._unMounted) {
+      this.setState({loading: true}, this.props.reload);
+    }
+  }
+
+  getNewSelection() {
+    if (!this._bridge || !this.props.getNewSelection) {
+      return;
+    }
+    this.props.getNewSelection(this._bridge);
+  }
+
+  sendSelection(id: string) {
+    if (!this._bridge || (!id && !this._store.selected)) {
+      return;
+    }
+    this.props.selectElement(id || this._store.selected, this._bridge);
+  }
+
+  inspectComponent(vbl: string) {
+    this.props.showComponentSource(vbl || '$r');
+  }
+
+  viewSource(id: string) {
+    if (!this._bridge) {
+      return;
+    }
+    this._bridge.send('putSelectedInstance', id);
+    setTimeout(() => {
+      this.props.showComponentSource('__REACT_DEVTOOLS_GLOBAL_HOOK__.$inst');
+    }, 100);
+  }
+
+  teardown() {
+    if (this._keyListener) {
+      window.removeEventListener('keydown', this._keyListener);
+      this._keyListener = null;
+    }
+    if (this._bridge) {
+      this._bridge.send('shutdown');
+    }
+    if (this._teardownWall) {
+      this._teardownWall();
+      this._teardownWall = null;
+    }
+    this._bridge = null;
+  }
+
+  inject() {
+    this.props.inject((wall, teardown) => {
+      this._teardownWall = teardown;
+
+      this._bridge = new Bridge();
+      // $FlowFixMe flow thinks `this._bridge` might be null
+      this._bridge.attach(wall);
+
+      // xx FlowFixMe this._bridge is not null
+      if (this._bridge) {
+        this._store = new Store(this._bridge);
+      }
+      this._keyListener = keyboardNav(this._store, window);
+
+      window.addEventListener('keydown', this._keyListener);
+
+      this._store.on('connected', () => {
+        this.setState({loading: false});
+        this.getNewSelection();
+      });
+    });
+  }
+
+  componentDidUpdate() {
+    if (!this.state.isReact) {
+      if (!this._checkTimeout) {
+        this._checkTimeout = setTimeout(() => {
+          this._checkTimeout = null;
+          this.lookForReact();
+        }, 200);
+      }
+    }
+  }
+
+  lookForReact() {
+    this.props.checkForReact(isReact => {
+      if (isReact) {
+        this.setState({isReact: true, loading: true});
+        this.inject();
+      } else {
+        console.log('still looking...');
+        this.setState({isReact: false, loading: false});
+      }
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div style={styles.loading}>
+          <h1>Connecting to react...</h1>
+          <br/>
+          If this is React Native, you need to interact with the app (just tap the screen) in order to establish the bridge.
+        </div>
+      );
+    }
+    if (!this.state.isReact) {
+      return <span>Looking for react...</span>;
+    }
+    return (
+      <Container
+        reload={this.reload.bind(this)}
+        menuItems={{
+          attr: (id, node, val, path, name) => {
+            if (!val || node.get('nodeType') !== 'Composite' || val[consts.type] !== 'function') {
+              return;
+            }
+            return [this.props.showAttrSource && {
+              title: 'Show Source',
+              action: () => this.props.showAttrSource(path),
+            }, this.props.executeFn && {
+              title: 'Execute function',
+              action: () => this.props.executeFn(path),
+            }];
+          },
+          tree: (id, node) => {
+            return [this.props.showComponentSource && node.get('nodeType') === 'Composite' && {
+              title: 'Show Source',
+              action: () => this.viewSource(id),
+            }, this.props.selectElement && this._store.capabilities.dom && {
+              title: 'Show in Elements Pane',
+              action: () => this.sendSelection(id),
+            }];
+          },
+        }}
+        extraPanes={this._store.capabilities.rnStyle && [panelRNStyle(this._bridge)]}
+      />
+    );
+  }
+}
+
+Panel.childContextTypes = {
+  store: React.PropTypes.object,
+};
+
+var panelRNStyle = bridge => (node, id) => {
+  var props = node.get('props');
+  if (!props || !props.style) {
+    return <strong>No style</strong>;
+  }
+  return (
+    <div>
+      <h3>React Native Style Editor</h3>
+      <NativeStyler id={id} bridge={bridge} />
+    </div>
+  );
+};
+
+var styles = {
+  chromePane: {
+    display: 'flex',
+  },
+  stretch: {
+    flex: 1,
+  },
+  loading: {
+    textAlign: 'center',
+    color: '#888',
+    padding: 30,
+    flex: 1,
+  },
+};
+
+module.exports = Panel;
+
---
+++ shells/chrome/src/panel.js
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
+'use strict';
+
+var checkForReact = require('./panel/checkForReact');
+var inject = require('./panel/inject');
+
+type Listenable = {
+  addListener: (fn: (message: Object) => void) => void,
+}
+
+type Port = {
+  disconnect: () => void,
+  onMessage: Listenable,
+  onDisconnect: Listenable,
+  postMessage: (data: Object) => void,
+};
+
+declare var chrome: {
+  devtools: {
+    network: {
+      onNavigated: {
+        addListener: (fn: () => void) => void,
+        removeListener: (fn: () => void) => void,
+      },
+    },
+    inspectedWindow: {
+      eval: (code: string, cb?: (res: any, err: ?Object) => any) => void,
+      tabId: number,
+    },
+  },
+  runtime: {
+    getURL: (path: string) => string,
+    connect: (config: Object) => Port,
+  },
+};
+
+var config = {
+  reload,
+  checkForReact,
+  reloadSubscribe(reload) {
+    chrome.devtools.network.onNavigated.addListener(reload);
+    return () => {
+      chrome.devtools.network.onNavigated.removeListener(reload);
+    };
+  },
+  getNewSelectiong(bridge) {
+    chrome.devtools.inspectedWindow.eval('window.__REACT_DEVTOOLS_GLOBAL_HOOK__.$0 = $0');
+    bridge.send('checkSelection');
+  },
+  selectElement(bridge, id) {
+    bridge.send('putSelectedNode', id);
+    setTimeout(() => {
+      chrome.devtools.inspectedWindow.eval('inspect(window.__REACT_DEVTOOLS_GLOBAL_HOOK__.$node)');
+    }, 100);
+  },
+  showComponentSource(vbl) {
+    var code = `Object.getOwnPropertyDescriptor(window.${vbl}.__proto__.__proto__, 'isMounted') &&
+      Object.getOwnPropertyDescriptor(window.${vbl}.__proto__.__proto__, 'isMounted').value ?
+        inspect(window.${vbl}.render) : inspect(window.${vbl}.constructor)`;
+    chrome.devtools.inspectedWindow.eval(code, (res, err) => {
+      if (err) {
+        console.error('Failed to inspect component', err);
+      }
+    });
+  },
+  showAttrSource(path) {
+    var attrs = '[' + path.map(m => JSON.stringify(m)).join('][') + ']';
+    var code = 'inspect(window.$r' + attrs + ')';
+    chrome.devtools.inspectedWindow.eval(code, (res, err) => {
+      if (err) {
+        console.error('Failed to inspect source', err);
+      }
+    });
+  },
+  executeFn(path) {
+    var attrs = '[' + path.map(m => JSON.stringify(m)).join('][') + ']';
+    var code = 'window.$r' + attrs + '()';
+    chrome.devtools.inspectedWindow.eval(code, (res, err) => {
+      if (err) {
+        console.error('Failed to call function', err);
+      }
+    });
+  },
+  inject(done) {
+    inject(chrome.runtime.getURL('build/backend.js'), () => {
+      var port = this._port = chrome.runtime.connect({
+        name: '' + chrome.devtools.inspectedWindow.tabId,
+      });
+      var disconnected = false;
+
+      var wall = {
+        listen(fn) {
+          port.onMessage.addListener(message => fn(message));
+        },
+        send(data) {
+          if (disconnected) {
+            return;
+          }
+          port.postMessage(data);
+        },
+      };
+
+      port.onDisconnect.addListener(() => {
+        disconnected = true;
+      });
+      done(wall, () => port.disconnect());
+    });
+  },
+};
+
+
+var globalHook = require('../../../backend/GlobalHook');
+globalHook(window);
+var Panel = require('./magic');
+var React = require('react');
+
+var node = document.getElementById('container');
+
+function reload() {
+  setTimeout(() => {
+    React.unmountComponentAtNode(node);
+    node.innerHTML = '';
+    React.render(<Panel {...config} />, node);
+  }, 100);
+}
+
+React.render(<Panel alreadyFoundReact={true} {...config} />, node);
+
+
+
