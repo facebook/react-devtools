ad35703c2817725f4aa5287a51ddc44ad80d48ac
Jan Kassens <jkassens@fb.com>
2015-10-02 11:32:17 -0700

    Reduce indentation in installGlobalHook

--- backend/installGlobalHook.js
+++ backend/installGlobalHook.js
@@ -17,47 +17,48 @@ import type {Hook} from './types';
  *       function in some places and inject the source into the page.
  */
 function installGlobalHook(window: Object) {
-  if (!window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
-    Object.defineProperty(window, '__REACT_DEVTOOLS_GLOBAL_HOOK__', {
-      value: ({
-        _renderers: {},
-        helpers: {},
-        inject: function(renderer) {
-          var id = Math.random().toString(16).slice(2);
-          this._renderers[id] = renderer;
-          this.emit('renderer', {id, renderer});
-        },
-        _listeners: {},
-        sub: function(evt, fn) {
-          this.on(evt, fn);
-          return () => this.off(evt, fn);
-        },
-        on: function(evt, fn) {
-          if (!this._listeners[evt]) {
-            this._listeners[evt] = [];
-          }
-          this._listeners[evt].push(fn);
-        },
-        off: function(evt, fn) {
-          if (!this._listeners[evt]) {
-            return;
-          }
-          var ix = this._listeners[evt].indexOf(fn);
-          if (ix !== -1) {
-            this._listeners[evt].splice(ix, 1);
-          }
-          if (!this._listeners[evt].length) {
-            this._listeners[evt] = null;
-          }
-        },
-        emit: function(evt, data) {
-          if (this._listeners[evt]) {
-            this._listeners[evt].map(fn => fn(data));
-          }
-        },
-      }: Hook),
-    });
+  if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    return;
   }
-};
+  Object.defineProperty(window, '__REACT_DEVTOOLS_GLOBAL_HOOK__', {
+    value: ({
+      _renderers: {},
+      helpers: {},
+      inject: function(renderer) {
+        var id = Math.random().toString(16).slice(2);
+        this._renderers[id] = renderer;
+        this.emit('renderer', {id, renderer});
+      },
+      _listeners: {},
+      sub: function(evt, fn) {
+        this.on(evt, fn);
+        return () => this.off(evt, fn);
+      },
+      on: function(evt, fn) {
+        if (!this._listeners[evt]) {
+          this._listeners[evt] = [];
+        }
+        this._listeners[evt].push(fn);
+      },
+      off: function(evt, fn) {
+        if (!this._listeners[evt]) {
+          return;
+        }
+        var ix = this._listeners[evt].indexOf(fn);
+        if (ix !== -1) {
+          this._listeners[evt].splice(ix, 1);
+        }
+        if (!this._listeners[evt].length) {
+          this._listeners[evt] = null;
+        }
+      },
+      emit: function(evt, data) {
+        if (this._listeners[evt]) {
+          this._listeners[evt].map(fn => fn(data));
+        }
+      },
+    }: Hook),
+  });
+}
 
 module.exports = installGlobalHook;
