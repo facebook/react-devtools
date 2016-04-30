8577b9d9af2bcd7974ffcd8687065a5f486bac29
Timothée Boucher <timothee.boucher@adroll.com>
2016-04-29 23:06:11 -0700

    Add keys for good measure in second similar part

--- frontend/Node.js
+++ frontend/Node.js
@@ -193,9 +193,9 @@ class Node extends React.Component {
         <span style={styles.tagText}>
           <span style={styles.openTag}>
             <span style={tagStyle}>&lt;{'' + node.get('name')}</span>
-            {node.get('key') && <Props props={{'key': node.get('key')}}/>}
-            {node.get('ref') && <Props props={{'ref': node.get('ref')}}/>}
-            {node.get('props') && <Props props={node.get('props')}/>}
+            {node.get('key') && <Props key='key' props={{'key': node.get('key')}}/>}
+            {node.get('ref') && <Props key='ref' props={{'ref': node.get('ref')}}/>}
+            {node.get('props') && <Props key='props' props={node.get('props')}/>}
             <span style={tagStyle}>&gt;</span>
           </span>
           {collapsed && '…'}
