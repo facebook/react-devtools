f8afe06d36c5018964ce41bad361299d603f302b
Kuu Miyazaki <miyazaqui@gmail.com>
2016-12-19 00:24:15 +0900

    Remove redundant code injected in Chrome

--- shells/chrome/src/GlobalHook.js
+++ shells/chrome/src/GlobalHook.js
@@ -32,6 +32,6 @@ var js = (
 // This script runs before the <head> element is created, so we add the script
 // to <html> instead.
 var script = document.createElement('script');
-script.textContent = js + saveNativeValues;
+script.textContent = js;
 document.documentElement.appendChild(script);
 script.parentNode.removeChild(script);
