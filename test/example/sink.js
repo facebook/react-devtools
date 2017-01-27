786b5f20cc052d10000b0ebe862d92dfe7e29009
Dan Abramov <dan.abramov@gmail.com>
2017-01-27 23:49:16 +0000

    Use non-Fiber examples since Fiber is not on npm

--- test/example/sink.js
+++ test/example/sink.js
@@ -12,7 +12,7 @@
 'use strict';
 
 var React = require('react');
-var ReactDOM = require('react-dom/fiber');
+var ReactDOM = require('react-dom');
 
 var assign = require('object-assign');
 var guid = require('../../utils/guid');
