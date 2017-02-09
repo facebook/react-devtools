93d8137ef18ce55fbe75112ac877be54871d56f1
Dan Abramov <dan.abramov@gmail.com>
2017-02-09 15:28:29 +0000

    [Electron] Make 2.0 backwards compatible with old RN (#500)
    * [Electron] Make 2.0 backwards compatible with old RN
    
    * Clarify event

--- packages/react-devtools-core/src/standalone.js
+++ packages/react-devtools-core/src/standalone.js
@@ -57,8 +57,19 @@ function onError(e) {
 
 function initialize(socket) {
   var listeners = [];
+  var isLegacyRN = false;
   socket.onmessage = (evt) => {
     if (evt.data === 'attach:agent') {
+      // <hack>
+      // TODO: This branch (and vendor/backend-1.0.6.js) can be removed when we're comfortable dropping
+      // support for RN <= 0.42 in Nuclide RN inspector. We used to send backend to RN to `eval`
+      // but in newer RN versions we just import the backend from npm package.
+      // We use `attach:agent` to detect old RN versions because we don't send it anymore.
+      if (!isLegacyRN) {
+        isLegacyRN = true;
+        socket.send('eval:' + fs.readFileSync(path.join(__dirname, '../vendor/backend-1.0.6.js')));
+      }
+      // </hack>
       return;
     }
     var data = JSON.parse(evt.data);
