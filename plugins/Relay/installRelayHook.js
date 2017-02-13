e693635f3de03b61e06fe2acbbdca182fa71b09a
Jason Quense <monastic.panic@gmail.com>
2017-02-13 15:33:03 -0500

    Allow reassignment of the relay install hook
    Lets the property be reassigned, so other consumers beside the devtools can instrument relay.
    
    I realize this is mostly secret, but It's super helpful as a general instrumentation hook. In particular I'm tying into this to improve e2e testing. Moving tests along when Relay is done making requests is a good bit nicer then arbitrary `.sleep()` commands.
    
    :)

--- plugins/Relay/installRelayHook.js
+++ plugins/Relay/installRelayHook.js
@@ -132,6 +132,7 @@ function installRelayHook(window: Object) {
 
   var _relayInternals = null;
   Object.defineProperty(hook, '_relayInternals', ({
+    writable: true,
     set: function(relayInternals) {
       _relayInternals = instrumentRelayRequests(relayInternals);
     },
