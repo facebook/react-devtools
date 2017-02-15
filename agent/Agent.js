af4bcc763611e70dae219f602741f9ba8574f546
Peter Newnham <github@blarg.co.uk>
2017-02-15 16:17:36 +0000

    Don't overwrite global $r if it is already set (#463)
    * Don't overwrite global $r if it is already set
    
    * Only set global $r if it has not yet been set or was last set by
    devtools itself

--- agent/Agent.js
+++ agent/Agent.js
@@ -103,10 +103,12 @@ class Agent extends EventEmitter {
     this.elementData = new Map();
     this.roots = new Set();
     this.reactInternals = {};
+    var lastSelected;
     this.on('selected', id => {
       var data = this.elementData.get(id);
-      if (data && data.publicInstance) {
+      if (data && data.publicInstance && this.global.$r === lastSelected) {
         this.global.$r = data.publicInstance;
+        lastSelected = data.publicInstance;
       }
     });
     this._prevSelected = null;
