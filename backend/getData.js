29d63e1e02ba64eb6dc4b4a2703889751f634f75
Ben Alpert <balpert@fb.com>
2017-04-21 12:55:14 -0700

    Make $r work for host nodes
    Test Plan: Checked in plain shell.

--- backend/getData.js
+++ backend/getData.js
@@ -71,6 +71,7 @@ function getData(internalInstance: Object): DataType {
     ref = internalInstance._currentElement.ref;
     if (typeof type === 'string') {
       name = type;
+      publicInstance = internalInstance.getPublicInstance();
     } else if (typeof type === 'function') {
       nodeType = 'Composite';
       name = getDisplayName(type);
--- frontend/PropState.js
+++ frontend/PropState.js
@@ -86,12 +86,13 @@ class PropState extends React.Component {
     var state = this.props.node.get('state');
     var context = this.props.node.get('context');
     var propsReadOnly = !this.props.node.get('canUpdate');
-    var isComposite = this.props.node.get('nodeType') === 'Composite';
+    var nodeType = this.props.node.get('nodeType');
+    var hasDollarR = nodeType === 'Composite' || nodeType === 'Native';
 
     return (
       <DetailPane
         header={'<' + this.props.node.get('name') + '>'}
-        hint={isComposite ? '($r in the console)' : null}>
+        hint={hasDollarR ? '($r in the console)' : null}>
         {key &&
           <DetailPaneSection
             title="Key"
