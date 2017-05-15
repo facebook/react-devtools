9d96be94c9d1895ef4e8ee3158a107693bf95ba9
Brian Vaughn <bvaughn@fb.com>
2017-05-14 20:18:11 -0700

    Fixed preferences popup for older Firefox versions

--- frontend/PreferencesPanel.js
+++ frontend/PreferencesPanel.js
@@ -220,6 +220,8 @@ const styles = {
     position: 'absolute',
     width: '100%',
     height: '100%',
+    top: 0,
+    left: 0,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
