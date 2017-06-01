125e020f35afedc251ec592f0f163ad3c50fb5cb
Brian Vaughn <bvaughn@fb.com>
2017-06-01 11:20:39 -0700

    Themes lintfix + alpha sort

--- frontend/Themes/Themes.js
+++ frontend/Themes/Themes.js
@@ -200,6 +200,31 @@ const FirefoxDark: Theme = {
   state05: '#181d20',
 };
 
+const FirefoxFirebug: Theme = {
+  displayName: 'Firefox (firebug)',
+  hidden: true,
+  base00: '#ffffff',
+  base01: '#f5f5f5',
+  base02: '#dde1e4',
+  base03: '#c1c1c1',
+  base04: '#888888',
+  base05: '#2a2a2a',
+  special00: '#0000ff',
+  special01: '#ff0000',
+  special02: '#ff0000',
+  special03: '#292e33',
+  special04: '#ff0000',
+  special05: '#0000ff',
+  special06: '#000062',
+  special07: '#0000ff',
+  state00: '#3399ff',
+  state01: '#85c0fb',
+  state02: '#ffffff',
+  state03: '#e6e6e6',
+  state04: '#ffee99',
+  state05: '#000000',
+};
+
 const FirefoxLight: Theme = {
   displayName: 'Firefox (light)',
   hidden: true,
@@ -224,31 +249,6 @@ const FirefoxLight: Theme = {
   state04: '#FFFF00',
   state05: '#585959',
 };
-
-const FirefoxFirebug: Theme = {
-  displayName: "Firefox (Firebug)",
-  hidden: true,
-  base00: "#ffffff",
-  base01: "#f5f5f5",
-  base02: "#dde1e4",
-  base03: "#c1c1c1",
-  base04: "#888888",
-  base05: "#2a2a2a",
-  special00: "#0000ff",
-  special01: "#ff0000",
-  special02: "#ff0000",
-  special03: "#292e33",
-  special04: "#ff0000",
-  special05: "#0000ff",
-  special06: "#000062",
-  special07: "#0000ff",
-  state00: "#3399ff",
-  state01: "#85c0fb",
-  state02: "#ffffff",
-  state03: "#e6e6e6",
-  state04: "#ffee99",
-  state05: "#000000",
-}
 
 const Flat: Theme = {
   displayName: 'Flat',
@@ -474,8 +474,8 @@ module.exports = {
   Dracula,
   Eighties,
   FirefoxDark,
-  FirefoxLight,
   FirefoxFirebug,
+  FirefoxLight,
   Flat,
   GruvboxDark,
   GruvboxLight,
