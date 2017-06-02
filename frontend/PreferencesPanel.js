6ca9aae84e0356707018d968478b6532205e6bc2
Jhen <developer@jhen.me>
2017-06-02 23:15:49 +0800

    PreferencesPanel: Show divider in dropdown if browserName not empty

--- frontend/PreferencesPanel.js
+++ frontend/PreferencesPanel.js
@@ -92,7 +92,7 @@ class PreferencesPanel extends React.Component {
             >
               {browserName && (<option value="">{browserName}</option>)}
               {hasCustomTheme && (<option value={CUSTOM_THEME_NAME}>Custom</option>)}
-              {(!showHiddenThemes || hasCustomTheme) && <option disabled="disabled">---</option>}
+              {(browserName || hasCustomTheme) && <option disabled="disabled">---</option>}
               {themeNames.map(key => (
                 <option key={key} value={key}>{themes[key].displayName}</option>
               ))}
