788417b401b244f4be7d8499c94c38f204d388d0
Tushar Sonawane <tusharlsonawane@gmail.com>
2017-06-06 12:23:13 +0530

    generating manifest file from template

--- shells/chrome/build.js
+++ shells/chrome/build.js
@@ -1,70 +1,40 @@
 #!/usr/bin/env node
-var fs = require('fs');  // file system
+var fs = require('fs-extra');  // file system
 var path = require('path');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config');
 var webpackBackendConfig = require('./webpack.backend');
+var packageJson = require('../../package.json');
+var manifestTemplateJson = require('./manifest.template.json');
 
-function copyFile(source, destination) {
-  var readstream = fs.createReadStream(source);
-  var writestream = fs.createWriteStream(path.join(destination, source));
-  readstream.pipe(writestream);
-}
-
-function removeDirectory(dirPath) {
-  try {
-    var filesToDelete = fs.readdirSync(dirPath);
-  } catch (e) {
-    return;
-  }
-  if (filesToDelete.length > 0) {
-    for (var i = 0; i < filesToDelete.length; i++) {
-      var filePath = dirPath + '/' + filesToDelete[i];
-      if (fs.statSync(filePath).isFile()) {
-        fs.unlinkSync(filePath);
-      } else {
-        removeDirectory(filePath);
-      }
-    }
-  }
-  fs.rmdirSync(dirPath);
-}
-
-function createDirectories(paths) {
-  paths.forEach(function(p) {
-    if (!fs.existsSync(p)) {
-      fs.mkdirSync(p);
-    }
-  });
-}
+main().then(function() {
+  console.log('extension is bundled at \'/extension\'');
+});
 
 function main() {
   var paths = {
     build: 'build',
-    dest: 'build/react-devtools-chrome',
-    destBuild: 'build/react-devtools-chrome/build',
-    destIcons: 'build/react-devtools-chrome/icons',
+    dest: 'extension',
+    destBuild: 'extension/build',
+    destIcons: 'extension/icons',
   };
 
   var files = [
-    'build/backend.js',
-    'build/background.js',
-    'build/contentScript.js',
-    'build/inject.js',
-    'build/main.js',
-    'build/panel.js',
-    'icons/icon48.png',
-    'icons/icon128.png',
+    'build',
+    'icons',
+    'popups',
     'main.html',
-    'manifest.json',
     'panel.html',
   ];
 
   process.env.NODE_ENV = JSON.stringify('production');
 
-  console.log('bundling extension...');
+  fs.emptyDirSync(paths.dest);
+
+  var generateManifest = generateManifestJson(paths.dest);
   
   var bundleFiles = new Promise(function(resolve, reject) {
+    // console.log('bundling it all together...');
     webpack([webpackConfig, webpackBackendConfig], function(error, stats) {
       if (!error) {
         resolve(stats);
@@ -74,29 +44,30 @@ function main() {
     });
   });
 
-  bundleFiles
-    .then(function() {
-      // check if directory exists
-      if (fs.existsSync(paths.dest)) {
-        // clean it up
-        removeDirectory(paths.dest);
-      }
-      
-      createDirectories([
-        paths.build,
-        paths.dest,
-        paths.destBuild,
-        paths.destIcons,
-      ]);
-      
-      files.forEach(function(file) {
-        copyFile(file, paths.dest);
-      });
-      console.log('extension bundled at \'build/react-devtools-chrome\'')
-    })
-    .catch(function(error) {
-      console.log('failed to bundle extension');
-    });
+  return Promise.all([
+    generateManifest,
+    bundleFiles,
+    fs.ensureDir(paths.destBuild),
+    fs.ensureDir(paths.destIcons),
+  ])
+  .then(function() {  
+    return Promise.all(
+      files.map(function(file) {
+        return fs.copy(file, paths.dest + '/' + file);
+      })
+    );
+  })
+  .catch(function(error) {
+    console.log('failed to bundle extension', error);
+  });
 }
 
-main();
+function generateManifestJson(destination) {
+  // console.log('generating manifest file...');
+  const manifest = Object.assign(manifestTemplateJson, {
+    'version': packageJson.version,
+  });
+
+  const fileToWrite = path.resolve(__dirname, destination, 'manifest.json');
+  return fs.writeJson(fileToWrite, manifest);
+}
