bba12810107adf93bb8372c90568936353c2cef2
pd4d10 <pd4d10@gmail.com>
2017-07-11 10:43:43 +0800

    Avoid global variable `script`

--- shells/webextension/src/inject.js
+++ shells/webextension/src/inject.js
@@ -15,10 +15,12 @@
 module.exports = function(scriptName: string, done: () => void) {
   var src = `
   // the prototype stuff is in case document.createElement has been modified
-  var script = document.constructor.prototype.createElement.call(document, 'script');
-  script.src = "${scriptName}";
-  document.documentElement.appendChild(script);
-  script.parentNode.removeChild(script);
+  (function () {
+    var script = document.constructor.prototype.createElement.call(document, 'script');
+    script.src = "${scriptName}";
+    document.documentElement.appendChild(script);
+    script.parentNode.removeChild(script);
+  })()
   `;
 
   chrome.devtools.inspectedWindow.eval(src, function(res, err) {
