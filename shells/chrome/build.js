607c5e57fab077400655ffd05a01bbebe2a87fb0
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-07-17 13:16:34 -0700

    Removed manual Chrome ext testing instructions Keeping things simplified, like Firefox does

--- shells/chrome/build.js
+++ shells/chrome/build.js
@@ -11,7 +11,7 @@ const {join} = require('path');
 const build = require('../webextension/build');
 
 const main = async () => {
-  const unpackedPath = await build(
+  await build(
     'chrome',
     join(__dirname, 'manifest.json'),
     join(__dirname, 'build')
@@ -21,11 +21,6 @@ const main = async () => {
   console.log(chalk.green('You can test this build by running:'));
   console.log(chalk.gray('\n# From the react-devtools root directory:'));
   console.log('npm run test:chrome');
-  console.log(chalk.green('\nYou can also test by following the steps below:'));
-  console.log(chalk.gray('\n# Open the following URL in Chrome:'));
-  console.log('chrome://extensions/');
-  console.log(chalk.gray('\n# Click "Load unpacked extension" and browse to:'));
-  console.log(unpackedPath);
 };
 
 main();
