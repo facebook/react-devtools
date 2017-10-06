d8e11bd70a006937c9ee5782cc723457944bf7cb
Raul Pineda <raul@raulpineda.mx>
2017-10-06 16:05:32 +0200

    use __dirname instead of process.env.PWD for build relative path

--- shells/webextension/build.js
+++ shells/webextension/build.js
@@ -21,7 +21,7 @@ const STATIC_FILES = [
   'panel.html',
 ];
 
-const relativePath = path => relative(process.env.PWD, path);
+const relativePath = path => relative(__dirname, path);
 
 const logPromise = async (promise, text, completedLabel = '') => {
   const {frames, interval} = dots;
