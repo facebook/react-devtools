c4f947d742d743b8b1f2ddf2502dd3dceeccd4ba
James Freeman <jfreeman@realmatters.com>
2018-01-18 16:13:25 -0500

    Prevent error if global hook couldn't run
    On pages with a CSP that prevents loading of scripts at self the global
    hook will fail. This is because we create a script tag and then inject
    it into the page itself.
    
    When this happens window.__REACT_DEVTOOLS_GLOBAL_HOOK__ will be
    undefined, and our checks for react will throw an exception.

--- shells/webextension/src/checkForReact.js
+++ shells/webextension/src/checkForReact.js
@@ -14,7 +14,7 @@
 
 module.exports = function(done: (pageHasReact: boolean) => void) {
   chrome.devtools.inspectedWindow.eval(`!!(
-    Object.keys(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers).length || window.React || (window.require && (require('react') || require('React')))
+    (window.__REACT_DEVTOOLS_GLOBAL_HOOK__ && Object.keys(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers).length) || window.React || (window.require && (require('react') || require('React')))
   )`, function(pageHasReact, err) {
     done(pageHasReact);
   });
