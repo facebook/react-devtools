245683db85fae313880595316ced71e640cd9844
Brian Vaughn <bvaughn@fb.com>
2018-05-30 11:46:01 -0700

    ProfilerManager turns profiling on/off for roots

--- plugins/Profiler/ProfilerManager.js
+++ plugins/Profiler/ProfilerManager.js
@@ -21,13 +21,15 @@ type Snapshot = {
   name: string,
 };
 
-type Snapshots = {[commitTime: number]: Array<Snapshot>};
+// TODO (bvaughn) Should this live in a shared constants file like ReactSymbols?
+// Or should it be in a Fiber-specific file somewhere (like getData)?
+const ProfileMode = 0b100;
 
 class ProfilerManager {
   _agent: Agent;
   _commitTime: number = 0;
   _isRecording: boolean = false;
-  _snapshots: Snapshots = {};
+  _snapshots: {[commitTime: number]: Array<Snapshot>} = {};
 
   constructor(agent: Agent) {
     this._agent = agent;
@@ -59,15 +61,40 @@ class ProfilerManager {
       actualDuration: data.profilerData.actualDuration,
       actualStartTime: data.profilerData.actualStartTime,
       baseTime: data.profilerData.baseTime,
-      commitTime: this._commitTime, // TODO This is redundant. Maybe ditch it?
+      commitTime: this._commitTime, // TODO (bvaughn) This is redundant. Maybe ditch it?
       name: data.name,
     });
   };
 
   _onIsRecording = isRecording => {
     this._isRecording = isRecording;
+
+    // Flip ProfilerMode on or off for each root.
+    // This instructs React to collect profiling data for the tree.
+    this._agent.roots.forEach(id => {
+      const root = this._agent.internalInstancesById.get(id);
+
+      // TODO (bvaughn) This check is flimsy.
+      if (root.name === 'Profiler') {
+        return;
+      }
+      
+      if (isRecording) {
+        root.mode |= ProfileMode; // eslint-disable-line no-bitwise
+        if (root.alternate !== null) {
+          root.alternate.mode |= ProfileMode; // eslint-disable-line no-bitwise
+        }
+      } else {
+        root.mode &= ~ProfileMode; // eslint-disable-line no-bitwise
+        if (root.alternate !== null) {
+          root.alternate.mode &= ~ProfileMode; // eslint-disable-line no-bitwise
+        }
+      }
+    });
+
+    // Dump snapshot data if we are done profiling.
     if (!isRecording) {
-      console.log(this._snapshots); // TODO Debugging only; remove this.
+      console.log(this._snapshots); // TODO (bvaughn) Debugging only; remove this.
       this._snapshots = {};
     }
   };
